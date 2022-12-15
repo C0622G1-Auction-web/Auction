b6b974dbf881ed7e96097fee4d49da8b48532096
minhtien100396 <minhtien100396@gmail.com>
2022-12-15 16:19:01 +0700

    TienBM update code front end

--- Auction/src/app/app-routing.module.ts
+++ Auction/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AuctionProductDetailComponent} from "./component/auction/auction-product-detail/auction-product-detail.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "auction/:productId", component: AuctionProductDetailComponent,
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- Auction/src/app/model/auction/auction.ts
+++ Auction/src/app/model/auction/auction.ts
@@ -1,2 +1,7 @@
 export interface Auction {
+  id?: number,
+  currentPrice: number,
+  auctionTime: string,
+  auctionStatus: boolean,
+  deleteStatus: boolean
 }
