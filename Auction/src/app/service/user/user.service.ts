d576b33642aad8c2895d439d3cd47dfa4370672d
unknown <qkietqn@gmail.com>
2022-12-15 16:01:21 +0700

    VietNQ, up prototype create user Angualla

--- Auction/src/app/service/user/user.service.ts
+++ Auction/src/app/service/user/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {User} from '../../model/user/user';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllUser(): Observable<User[]> {
+    return null;
+  }
 }
