d2c07a6368714f79982fff7c0827da3fb9c3dc11
Quang Anh <you@example.com>
2022-12-15 16:04:59 +0700

    form end history product

--- Auction/src/app/model/product/product.ts
+++ Auction/src/app/model/product/product.ts
@@ -1,2 +1,8 @@
 export interface Product {
+  nameProduct: String;
+  description: String;
+  RegisterDay: String;
+  Review: String;
+  IsDelete: String;
+
 }
--- Auction/src/app/service/product/product.service.ts
+++ Auction/src/app/service/product/product.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {DataResult} from "../../model/product/data_result";
+import {ProductDto} from "../../model/product/iProduct_dto";
+import {ReviewStatus} from "../../model/product/review_Status";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private API_URL = '  http://localhost:8080/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  findAll(curPage: number, numberRecord: number): Observable<DataResult<ProductDto>> {
+    return this.httpClient.get<DataResult<ProductDto>>(this.API_URL + 'list?page=' + (curPage - 1) + '&size=' + numberRecord );
+  }
+  findAllReview(): Observable<ReviewStatus[]> {
+    return this.httpClient.get<ReviewStatus[]>(this.API_URL + 'listReviewStatus');
+  }
+
 }
