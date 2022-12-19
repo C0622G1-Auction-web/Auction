29d8c204b30e71acaf3fcedc78045e4f95fa3522
hudagon <dohoangduc1112000@gmail.com>
2022-12-19 21:50:05 +0700

    DucDH update dev-feature-account

--- Auction/src/app/app-routing.module.ts
+++ Auction/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {GuideListComponent} from './component/guide/guide-list/guide-list.component';
 import {AuctionProductDetailComponent} from './component/auction/auction-product-detail/auction-product-detail.component';
 import {ProductAddComponent} from './component/product/product-add/product-add.component';
@@ -15,14 +15,15 @@ import {ProductReviewComponent} from './component/product/product-review/product
 import {RegisterWithGoogleComponent} from "./security/register-with-google/register-with-google.component";
 import {AddressPaymentComponent} from './component/payment/address-payment/address-payment.component';
 import {MethodPaymentComponent} from './component/payment/method-payment/method-payment.component';
-import { AuctionProductAddComponent } from './component/auction/auction-product-add/auction-product-add.component';
-import { LoginComponent } from './component/security/login/login.component';
+import {AuctionProductAddComponent} from './component/auction/auction-product-add/auction-product-add.component';
 import {GuideAddComponent} from "./component/guide/guide-add/guide-add.component";
 import {GuideEditComponent} from "./component/guide/guide-edit/guide-edit.component";
 import {ChatUserComponent} from "./component/chat/chat-user/chat-user.component";
 import {ChatAdminComponent} from "./component/chat/chat-admin/chat-admin.component";
 import {UserCreateComponent} from "./component/user/user-create/user-create.component";
 import {UserUpdateComponent} from "./component/user/user-update/user-update.component";
+import {LoginComponent} from "./security/login/login.component";
+
 
 const routes: Routes = [
   {
@@ -41,28 +42,14 @@ const routes: Routes = [
     path: "product/edit/:id",
     component: ProductEditComponent
   },
-
-  {
-    path: "home",
-    component: HomeComponent
-
-  },
   {
     path: "",
     component: HomeComponent
   },
-
   {
     path: 'transaction',
     component: TransactionComponent
   },
-  {
-    path: 'aa',
-    component: TransactionComponent
-  },
-
-  {path: 'transaction', component: TransactionComponent},
-
   {
     path: 'home',
     component: HomeComponent
@@ -80,10 +67,6 @@ const routes: Routes = [
     component: GuideListComponent
   },
   {
-    path: 'user-add',
-    component: UserAddComponent
-  },
-  {
     path: 'user/add',
     component: UserAddComponent
   },
@@ -92,30 +75,8 @@ const routes: Routes = [
     component: LockaccountUserComponent
   },
   {
-    path: 'product/add',
-    component: ProductAddComponent
-  }, {
-    path: 'guide',
-    component: GuideListComponent
-  },
-  {
-    path: 'auction/:productId', component: AuctionProductDetailComponent,
-
-  }
-,
-  {
-    path: 'product/add',
-    component: ProductAddComponent
-  }, {
-    path: 'guide',
-    component: GuideListComponent
-  },
-  {
     path: 'auction/product/add',
     component: AuctionProductAddComponent
-  }, {
-    path: 'guide',
-    component: GuideListComponent
   },
   {
     path: 'auction/:productId',
@@ -125,54 +86,42 @@ const routes: Routes = [
     path: 'confirmAddress',
     component: AddressPaymentComponent
   },
-
   {
     path: 'methodPayment',
     component: MethodPaymentComponent
+  },
+  {
+    path: 'guide/edit/:id',
+    component: GuideEditComponent
+  },
+  {
+    path: 'guide/add',
+    component: GuideAddComponent
+  },
+  {
+    path: 'auction/chat/user',
+    component: ChatUserComponent
+  },
+  {
+    path: 'auction/chat/support',
+    component: ChatAdminComponent
+  },
+  {
+    path: "user/create",
+    component: UserCreateComponent
+  },
+  {
+    path: 'user/update/:id',
+    component: UserUpdateComponent
   },
   {
     path: 'login',
     component: LoginComponent
   },
   {
-    path: 'methodPayment', component: MethodPaymentComponent
-  }, {path: 'login', component: LoginComponent},
-{
-  path: 'product/add',
-  component: ProductAddComponent
-},
-  {
-  path: 'guide',
-  component: GuideListComponent
-},
-  {
-    path: 'guide/edit/:id',
-    component: GuideEditComponent
-  },
-  {
-    path:'guide/add',
-    component:GuideAddComponent
-  },
-  {
-    path: "auction/:productId", component: AuctionProductDetailComponent,
-
-  },
-  {
-    path:'auction/chat/user',
-    component: ChatUserComponent
-  },
-  {
-    path:'auction/chat/support',
-    component: ChatAdminComponent
-  },
-  {
-    path: "auction/:productId", component: AuctionProductDetailComponent},
-  {
-    path:"user/create",component:UserCreateComponent
-  },
-  {path:'user/update/:id', component: UserUpdateComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'registerWithGoogle/:email', component: RegisterWithGoogleComponent}
+    path: 'registerWithGoogle/:email',
+    component: RegisterWithGoogleComponent
+  }
 ];
 
 @NgModule({
